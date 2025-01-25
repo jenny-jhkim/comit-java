37a3d42cfbcb9a633ae21714f78413fa47e79ecd
Jihyeon Kim <imjihyeon.kim@gmail.com>
2025-01-24 20:00:46 -0500

    first commit

---
+++ src/main/java/org/comit/course/_01_variables/Variable.java
@@ -0,0 +1,59 @@
+package org.comit.course._01_variables;
+
+import java.util.Scanner;
+
+public class Variable {
+	static final double PI = 3.14159;
+	
+	
+	/**
+	 * @param a
+	 * @param b
+	 */
+	void doSomething( int a, int b) {
+		System.out.println(a+b);
+	}
+
+	public static void main(String[] args) {
+		
+		//Primitives - integer
+		byte num1 = 100;
+		short num2 = -32_000;		
+		int num3 = 2_147_483_647;
+		long num4 = 1456;
+				
+		//decimal
+		float num5 = 123.45678901234567890f;
+		double num6 = 123.45678901234567890;
+		//System.out.println(num5);
+		//System.out.println(num6);
+		
+		char chr1 = 'A';
+		boolean boo1 = true;
+		boolean boo2 = false;
+		
+	
+		//Non-Primitives
+		String str = null;	
+		String str2 = new String("Hello");
+		String str3 = new String("Hello");
+//		System.out.println(str2==str3);
+		
+//		if (str == null) {
+//			System.out.println("Please provide str:");
+//			Scanner scan = new Scanner(System.in);
+//			str = scan.nextLine();
+//		}
+//		System.out.println("The new value of str is : " + str);
+
+		String str1 = "Hello";
+		System.out.println("str length: " + str1.length() );
+		System.out.println("str to upper case:" + str1.toUpperCase() );
+		
+	
+		int r = 3;
+		
+		System.out.println("circle area: " + PI*r*r);
+	}
+	
+}
---
+++ src/main/java/org/comit/practice/Practice01.java
@@ -0,0 +1,42 @@
+package org.comit.practice;
+
+public class Practice01
+{
+    public static void main( String[] args )
+    {
+    	//Ex1
+    	int num1 = 23452;
+    	int num2 = 45363;
+        System.out.println( num1 );
+        System.out.printf("%d %n%n", num2);
+        
+        //Ex2
+    	double num3 = 45.62435;
+    	double num4 = 107.25424;
+        System.out.println( num3 );
+        System.out.println( num4 );
+        System.out.println("");
+        
+        //Ex3
+        long num5 = 24_125_456_143L;
+        long num6 = 784125345656L;
+        System.out.println( num5 );
+        System.out.println( num6 );
+        System.out.println("");
+        
+        //Ex4
+        String str1 = "I love Java";
+        String str2 = "Hello ComIT";
+        System.out.println( str1 );
+        System.out.println( str2 );
+        System.out.println("");
+        
+        //Ex5
+        float num7 = 5.3f;
+        float num8 = 7.5f;
+        System.out.println( num7 );
+        System.out.printf("%.2f %n", num8);
+        
+    }
+}
+
